e3feba8af26f5c3539d8226cf90f8d6ea79c0e0e
Letalis <skipa-st@yandex.ru>
2016-11-05 15:36:39 +0300

    delete serverrun

--- bot.py
+++ bot.py
@@ -192,7 +192,7 @@ def remove_hook():
     return "Webhook has been removed"
 
 # Запуск сервера
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+#server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 webhook()
 
 
