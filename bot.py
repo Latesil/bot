537469176e72e3ab875e4d5bdcf4ebc1c21562cb
Letalis <skipa-st@yandex.ru>
2016-11-05 14:32:17 +0300

    fix error

--- bot.py
+++ bot.py
@@ -196,5 +196,5 @@ def remove_hook():
 webhook()
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True, interval=0)
+#if __name__ == '__main__':
+#    bot.polling(none_stop=True, interval=0)
